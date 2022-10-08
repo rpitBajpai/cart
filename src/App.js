74fd03751a9f95b6875beed6e5e5970a8b413b35
Arpit Bajpai <arpitbajpai4321@gmail.com>
2022-10-08 22:27:58 +0530

    creating the cart component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
-import CartItem from "./CartItem";
+import React from "react";
+// import CartItem from "./CartItem";
+import Cart from './Cart';
 
 function App() {
   return (
     <div className="App">
       
-      <CartItem />
+      <Cart />
     </div>
   );
 }
