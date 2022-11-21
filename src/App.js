e87d03724a50c0f5ee75673243dda183cb424c68
Arpit Bajpai <arpitbajpai4321@gmail.com>
2022-11-21 20:48:24 +0530

    Adding products in Firebase

--- src/App.js
+++ src/App.js
@@ -13,8 +13,11 @@ class App extends React.Component{
         products: [],
         loading: true
     }
+    
     // this.increaseQuantity = this.increaseQuantity.bind(this);
     // this.testing();
+
+    this.db = firebase.firestore();
 }
 
 componentDidMount(){
@@ -44,8 +47,7 @@ componentDidMount(){
   //   })
 
   // use onSnapshot() to update UI without refreshing the tab
-  firebase
-    .firestore()
+  this.db
     .collection('products')
     .onSnapshot((snapshot) => {
       console.log(snapshot);
@@ -131,11 +133,29 @@ getCartTotal = () => {
   return cartTotal;
 }
 
+addProduct = () => {
+  this.db
+    .collection('products')
+    .add({
+      img: '',
+      price: 900,
+      qty: 3,
+      title: 'washing machine'
+    })
+    .then((docRef) => {
+      console.log('Product has been added', docRef);
+    })
+    .catch((error) => {
+      console.log('Error : ', error);
+    })
+}
+
 render(){
   const {products, loading}= this.state;
   return (
     <div className="App">
       <Navbar count={this.getCartCount()}/>
+      <button onClick={this.addProduct} style={{ padding: 20, fontSize: 20 }}>Add a product</button>
       <Cart 
         products={products}
         onIncreaseQuantity={this.handleIncreaseQuantity}
